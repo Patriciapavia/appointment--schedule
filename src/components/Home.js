456195f4db7465e2c7459596c792ed9d5a23800d
patricia <patriciapavia236@gmail.com>
2021-11-17 17:36:07 +1100

    add filteron home pages to filter out unique coach name and id

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,14 +3,25 @@ import AvailableCoaches from './AvailableCoaches';
 import { Container } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchDataAction } from '../actions/appointmentTimeAction';
-import { appointmentDatas } from '../data/data';
-
-console.log(appointmentDatas);
 
 const Home = () => {
 	const dispatch = useDispatch();
 	const appointmentLists = useSelector((state) => state.appointmentLists);
 	const datas = appointmentLists.datas;
+
+	let coachLists = [];
+	if (datas) {
+		datas.filter(function (item) {
+			let i = coachLists.findIndex(
+				(x) => x.title === item.title && x.id === item.id
+			);
+			if (i <= -1) {
+				coachLists.push(item);
+			}
+			return null;
+		});
+	}
+
 	useEffect(() => {
 		dispatch(fetchDataAction());
 	}, [dispatch]);
@@ -18,8 +29,8 @@ const Home = () => {
 	return (
 		<Container maxWidth='sm'>
 			<h1>Available Coaches</h1>
-			{datas
-				? datas.map((coach) => <AvailableCoaches coach={coach} />)
+			{coachLists
+				? coachLists.map((coach) => <AvailableCoaches coach={coach} />)
 				: 'Loading'}
 		</Container>
 	);
