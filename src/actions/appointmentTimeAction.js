341e59ecfd7dc79f3254e49171c70719f0afef45
patricia <patriciapavia236@gmail.com>
2021-11-17 17:16:48 +1100

    creaded appointmentAction

---
+++ src/actions/appointmentTimeAction.js
@@ -0,0 +1,9 @@
+import { FETCH_DATA_SUCCESS } from '../constants/fetAppointmentConstant';
+import { appointmentDatas } from '../data/data';
+
+export const fetchDataAction = () => (dispatch) => {
+	dispatch({
+		type: FETCH_DATA_SUCCESS,
+		payload: appointmentDatas,
+	});
+};
